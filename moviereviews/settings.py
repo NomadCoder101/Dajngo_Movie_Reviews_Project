6dd1e930f8d8196f5bea31dad8208907ba39da35
NomadCoder101 <100473557+NomadCoder101@users.noreply.github.com>
2023-01-14 22:34:59 -0500

    Update settings.py

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -28,7 +28,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*hb63e$df%!-6p414h4+l_5*&_0&z*(u3e$9bqxw^j1#9s@lib'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
